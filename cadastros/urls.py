34f652b46477ab78e2fb395688c7f3f2ebce06e7
gbianchiprado <gabibianchiprado@gmail.com>
2024-05-06 22:28:06 -0300

    upload demais páginas

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from cadastros.views import home, consult
+from cadastros.views import home, consult, ap_input, closing, pending_docs
 
 urlpatterns = [
     path('', home),
-    path('consulta', consult)
+    path('consulta', consult),
+    path('lancamento', ap_input),
+    path('fechamento', closing),
+    path('documentospendentes', pending_docs)
 
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
-def consult(request):
+def ap_input(request):
     return render(request, 'pag002.html')
+
+def consult(request):
+    return render(request, 'pag003.html')
+
+def pending_docs(request):
+    return render(request, 'pag004.html')
+
+def closing(request):
+    return render(request, 'pag005.html')
 
 # Create your views here.
